e2e8a97db0b2d23cad59c30b216881f634c84934
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-10-22 12:23:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#Открываем файл,чтобы его прочитать
+with open('text.txt', 'r', encoding='utf-8') as file:
+    #Читаем содержимое файла
+    text = file.read()
+#Разобъём текст на слова, используя знаки препинания и пробелы  
+words = text.split()
+#Приводим слова к нижнему регистру и удаляем дубликаты с помощью множества
+unique_words = set(word.lower().strip(',.?!";:[]()') for word in words)
+#Открываем новый файл, куда будут записываться уникальные слова и числа
+with open('output.txt', 'w', encoding='utf-8') as output:
+    #Записываем уникальные слова в файл
+    for word in sorted(unique_words):
+        output.write(word + '  ')
+print("Уникальные слова записаны в output.txt")
