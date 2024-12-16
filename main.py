a6b2b858171737d358280dd2cc1dd53af6cdc3f7
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-12-16 20:45:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open('text.txt', 'r', encoding = 'utf-8') as file:
 words = text.split()
 #Приводим слова к нижнему регистру и удаляем дубликаты с помощью множества
 unique_words = set(word.lower().strip(',.?!";:[]()') for word in words)
-#Открываем новый файл, куда будут записываться уникальные слова и числа
+#Открываем новый файл, куда будут записываться уникальные слова и числа 
 with open('output.txt', 'w', encoding = 'utf-8') as output:
     #Записываем уникальные слова в файл
     for word in sorted(unique_words):
